49d1e76fb282bfadd84910ba5a15acc6c3a3f85d
miftajannat <miftajannat.dev@gmail.com>
2023-02-03 16:25:02 +0600

    added project

---
+++ data/projectdata.js
@@ -0,0 +1,37 @@
+const projectdata = [
+
+    {
+        id: 4,
+        title: "The Compound Effect",
+        demo: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        code: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        skill: "Python, React, SQL, Git",
+        image:"https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fd4d6545c-f7cd-4b2a-9bbd-7ae902e73c28_354x500.jpeg"
+    },
+    {
+        id: 3,
+        title: "The Compound Effect",
+        demo: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        code: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        skill: "Python, React, SQL, Git",
+        image:"https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fd4d6545c-f7cd-4b2a-9bbd-7ae902e73c28_354x500.jpeg"
+    },
+    {
+        id: 2,
+        title: "The Compound Effect",
+        demo: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        code: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        skill: "Python, React, SQL, Git",
+        image:"https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fd4d6545c-f7cd-4b2a-9bbd-7ae902e73c28_354x500.jpeg"
+    },
+    {
+        id: 1,
+        title: "The Compound Effect",
+        demo: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        code: "https://booknote.substack.com/p/the-compound-effect-by-darren-hardy",
+        skill: "Python, React, SQL, Git",
+        image:"https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Fd4d6545c-f7cd-4b2a-9bbd-7ae902e73c28_354x500.jpeg"
+    },
+];
+
+export default projectdata;
---
+++ pages/project.js
@@ -0,0 +1,50 @@
+import React from "react";
+import projectdata from "../data/projectdata";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+const Project = () => {
+  return (
+    <div className='md:max-w-[1040px] sm:mx-5 md:mx-auto box-border'>
+      <NavBar />
+
+      <div className='text-4xl font-bold pt-10'>
+        <h1>Project Summaries</h1>
+        <hr className='border-2 border-blue-500 rounded-md border-solid w-[87px] my-4' />
+      </div>
+
+
+      <div className='grid grid-cols-1  md:grid-cols-2 py-2 gap-16 pb-20'>
+        {projectdata.map((project) => (
+          <div key={project.id}>
+            <div>
+              <div className='flex justify-center gap-4'>
+                <img
+                  className='mr-3 h-[226px] w-[250px] shadow-xl rounded-md box-border transition-colors duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg'
+                  src={project.image}
+                  alt={project.title}
+                />
+
+                <div className='items-center h-[225px] overflow-hidden'>
+                  <p className='text-2xl font-semibold pb-1'>
+                    {project.title}
+                  </p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, sint.</p>
+                  <p className="pb-3">{project.skill}</p>
+
+                  <p className='text-xl font-bold text-blue-500 underline'><a href={project.demo}>Demo</a></p>
+                  <p className="text-xl text-blue-500 font-bold underline"><a href={project.code}>Code</a></p>
+                 
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Project;
